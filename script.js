533e8389d5bcdcae9d67593f7af3175fa3477240
Caleb Justice <calebjustice@Calebs-MBP.brywhi.com>
2019-10-09 19:44:20 -0600

    delete lists working

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function modal() {
+    var modal = document.getElementById("modal-container");
+    modal.style.display = "block";
+    document.getElementById("close").addEventListener("click", function(){
+        modal.style.display = "none";
+    })
+}
+
 var lists = [];
 var selectedList = {};
 var id = 0;
@@ -20,42 +28,57 @@ function itemReady(){
         lists.push({
             name: input.value,
             iD: id,
-            tasks: []
+            tasks: [],
+            completed: false
         });
         renderLists(id);
     }
     id++;
 
+    input.value = "";
+
     for(let i = 0; i < document.getElementsByClassName("item").length; i++) {
-        document.getElementsByClassName("item")[i].addEventListener("click", function(){
+        document.getElementsByClassName("item")[i].addEventListener("click", function(event){
+            var activeClass = document.getElementsByClassName("listItemActive");
+            for(let j = 0; j < activeClass.length; j++) {
+                activeClass[j].classList.remove("listItemActive");
+            }
             selectedList.id = i;
+            selectedList.tasks = lists[selectedList.id].tasks;
+            var x = event.target;
+            x.parentNode.classList.add("listItemActive");
             renderTasks();
         })
     }
 }
 
 function renderLists(id){
-    let html = "<div>";
+    let html = `<div class="list-container">`;
     for(list in lists) {
         html += `<div class="listItem">
                     <i class="fas fa-trash-alt" onclick="deleteList(${lists[list].iD})"></i>
                     <div class="item">${lists[list].name}</div>
                 </div>`;
     }
-    html += "</div>";
+    html += `</div>`;
     document.getElementById("lists").innerHTML = html;
 }
 
 function renderTasks() {
-    let tasksList = lists[selectedList.id].tasks;
     let html = "<div>";
-    for(tasks in tasksList) {
+    for(tasks in lists[selectedList.id].tasks) {
         html += `<div class="text">
-                    <div>${tasksList[tasks]}</div>
+                    <i onclick="deleteTask()" class="fas fa-trash-alt"></i>
+                    <i class="far fa-circle"></i>
+                    <div>${lists[selectedList.id].tasks[tasks]}</div>
                 </div>`;
     }
     html += "</div>";
     document.getElementById("tasks").innerHTML = html;
+}
+
+function deleteTask(){
+    console.log(selectedList.tasks);
 }
 
 function deleteList(id){
@@ -65,7 +88,11 @@ function deleteList(id){
             selectedList.id = 0;
         }
     }
+    if(lists.length > 1) {
+        renderTasks();
+    }
     renderLists();
+    itemReady();
 }
 
 document.getElementById("task-input").addEventListener("keyup", function(event) {
@@ -74,8 +101,11 @@ document.getElementById("task-input").addEventListener("keyup", function(event) 
     }
 })
 
+var completedClicks = 0;
+
 function addTask() {
-    let taskInput = document.getElementById("task-input").value;
-    lists[selectedList.id].tasks.push(taskInput);
+    let taskInput = document.getElementById("task-input");
+    lists[selectedList.id].tasks.push(taskInput.value);
     renderTasks();
+    taskInput.value = "";
 }
